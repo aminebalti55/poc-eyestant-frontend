3804fab8f7784cfed129f5476f4967201064588a
aminebalti115 <99841097+aminebalti115@users.noreply.github.com>
2024-06-17 02:17:05 +0100

    small_adjusments

--- src/app/components/Molecules/TableRowActions.jsx
+++ src/app/components/Molecules/TableRowActions.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import IconButton from '../Atoms/IconButton';
 
 const TableRowActions = ({ onDelete, index }) => (
-  <div className="flex justify-end space-x-2 pr-8">
+  <div className="flex justify-end space-x-2 pr-4">
     <IconButton icon="eye" className="text-gray-500 cursor-pointer" size={12} />
     <IconButton icon="key" className="text-gray-500 cursor-pointer" size={12} />
     <IconButton icon="minus" className="bg-red-600 text-white" size={8} onClick={() => onDelete(index)} />
